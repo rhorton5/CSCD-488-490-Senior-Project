5aac06773f746203c484a37038e479bcdcca8dbc
Ryley Horton <48656566+rhorton5@users.noreply.github.com>
2021-11-30 11:34:49 -0800

    Minor Fix
    Fixed a bug created where the program would always load the database regardless of pressing "Open Existing" button or "Create New" button.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team_6_Senior_Project
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        public static bool runViewAll = false;
+        public static string filenameViewAll = "None";
+        public static bool closeProgram = false;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            while (!closeProgram)
+            {
+                Application.Run(new MainMenu());
+
+                if (runViewAll)
+                {
+                    Application.Run(new TableMenu(filenameViewAll));
+                    Console.WriteLine(filenameViewAll);
+                }
+            }
+            
+            
+        }
+    }
+}
